62ed4f5b97eb850c15483c1d72d3b46b29e9e7d9
thehcker <isaacnyakoi1@gmail.com>
2019-05-17 13:32:41 +0300

    Edit the Album Image and file Fields

--- music/models.py
+++ music/models.py
@@ -1,13 +1,30 @@
+import random
+import os
 from django.db import models
 from django.urls import reverse
 
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+def upload_image_path(instance, filename):
+    new_filename = random.randint(1,3910209312)
+    name, ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+    return "music/{new_filename}/{final_filename}".format(
+            new_filename=new_filename, 
+            final_filename=final_filename
+            )
 
 # Create your models here.
 class Album(models.Model):
 	artist = models.CharField(max_length=100)
 	album_title = models.CharField(max_length=100)
 	genre = models.CharField(max_length=50)
-	album_logo = models.CharField(max_length=500)
+	album_logo = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
 	is_favorite = models.BooleanField(default=False)
 
 	def get_absolute_url(self):
@@ -19,7 +36,7 @@ class Album(models.Model):
 
 class Song(models.Model):
 	album = models.ForeignKey(Album, on_delete=models.CASCADE)
-	audio_file = models.CharField(max_length=200)
+	audio_file = models.FileField(default='')
 	song_title = models.CharField(max_length=100)
 	is_favorite = models.BooleanField(default=False)
 
--- songs/urls.py
+++ songs/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('music/', include('music.urls')),
+    path('', include('music.urls')),
     path('accounts/', include('accounts.urls')),
 ]
